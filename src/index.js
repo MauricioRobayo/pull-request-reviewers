cc60d76d0b4b71067a2d2602b88b8f97914a7623
Mauricio Robayo <rfmajo@gmail.com>
2019-11-15 19:32:36 -0500

    chore: initial interaction on console

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import './style.scss'
 import GitHubPR from './js/github-pr'
 
-// const pr = new GitHubPR('https://github.com/oscarnava/fakebook/pull/6')
-const pr = new GitHubPR('https://github.com/MauricioRobayo/weather-app/pull/1')
+async function onsubmit(event) {
+  event.preventDefault()
+  const prUrl = document.querySelector('#pull-request-url').value
+  const pr = new GitHubPR(prUrl)
+  const [info, reviewers] = await Promise.all([
+    pr.fetchInfo(),
+    pr.fetchReviewers(),
+  ])
+  // eslint-disable-next-line no-console
+  console.log(info, reviewers)
+}
 
-pr.fetchReviewers().then(response => response.map(console.log))
+document
+  .querySelector('#pull-request-form')
+  .addEventListener('submit', onsubmit)
