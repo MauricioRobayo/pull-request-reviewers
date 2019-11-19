ee44640de520a03959370780163d54d20abe787f
Mauricio Robayo <rfmajo@gmail.com>
2019-11-19 18:20:04 -0500

    style: initial header style

--- src/js/components/pr-info.js
+++ src/js/components/pr-info.js
@@ -1,9 +1,15 @@
+import octicons from '@primer/octicons'
 import * as create from '../element-creator'
 import Info from './info'
 
 export default class PRInfo extends Info {
   constructor({ title, url, state }) {
     super({ title, url })
+    this.prIcon = create.element('span', {
+      classList: ['icon'],
+      innerHTML: octicons['git-pull-request'].toSVG(),
+    })
+    this.title.prepend(this.prIcon)
     this.state = create.element('div', {
       textContent: state,
       classList: ['pr-state'],
