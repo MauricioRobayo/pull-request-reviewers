1f29d9a6004374a996af36d6de5c0753c61d9ebb
Mauricio Robayo <rfmajo@gmail.com>
2019-12-12 16:05:49 -0500

    test: update tests

--- src/js/github.test.js
+++ src/js/github.test.js
@@ -2,19 +2,23 @@ import githubPR from './github-pr'
 
 const validPrUrls = [
   {
-    prUrl: 'https://github.com/user/repo/pull/1',
+    prUrl: 'https://github.com/user/repo/pull/1/files',
+    expectedUrl: 'https://github.com/user/repo/pull/1',
     apiEndpoint: 'https://api.github.com/repos/user/repo/pulls/1',
   },
   {
-    prUrl: 'https://github.com/UserName/repo-1/pull/1',
+    prUrl: 'https://github.com/UserName/repo-1/pull/1#issue-350061414',
+    expectedUrl: 'https://github.com/UserName/repo-1/pull/1',
     apiEndpoint: 'https://api.github.com/repos/UserName/repo-1/pulls/1',
   },
   {
     prUrl: 'https://github.com/user-name/Repo2/pull/1',
+    expectedUrl: 'https://github.com/user-name/Repo2/pull/1',
     apiEndpoint: 'https://api.github.com/repos/user-name/Repo2/pulls/1',
   },
   {
     prUrl: 'https://github.com/user_name/repo_3/pull/1',
+    expectedUrl: 'https://github.com/user_name/repo_3/pull/1',
     apiEndpoint: 'https://api.github.com/repos/user_name/repo_3/pulls/1',
   },
 ]
@@ -26,43 +30,33 @@ const invalidPrUrls = [
   { prUrl: 'https://example.com/user/repo/pull/1' },
 ]
 
-describe('returns true for a valid GitHub url', () => {
-  validPrUrls.forEach(({ prUrl }) => {
-    test(`returns true for ${prUrl}`, () => {
-      expect(githubPR.isValidPRUrl(prUrl)).toBe(true)
-    })
-  })
-})
-
-describe('returns false for an invalid GitHub url', () => {
+describe('returns an empty string for an invalid GitHub url', () => {
   invalidPrUrls.forEach(({ prUrl }) => {
     test(`returns false for ${prUrl}`, () => {
-      expect(githubPR.isValidPRUrl(prUrl)).toBe(false)
+      expect(githubPR.validatePRUrl(prUrl)).toBe('')
     })
   })
 })
 
-describe('returns the same url for a valid GitHub url', () => {
-  validPrUrls.forEach(({ prUrl }) => {
-    test(`returns the same url for ${prUrl}`, () => {
-      expect(githubPR.validatePRUrl(prUrl)).toBe(prUrl)
+describe('returns the pr url for a valid GitHub url', () => {
+  validPrUrls.forEach(({ prUrl, expectedUrl }) => {
+    test(`returns the pr pr url for ${prUrl}`, () => {
+      expect(githubPR.validatePRUrl(prUrl)).toBe(expectedUrl)
     })
   })
 })
 
-describe('throws for an invalid GitHub url', () => {
+describe('returns an empty string for an invalid GitHub url', () => {
   invalidPrUrls.forEach(({ prUrl }) => {
-    test(`throws for ${prUrl}`, () => {
-      expect(() => {
-        githubPR.validatePRUrl(prUrl)
-      }).toThrow('Not a valid GitHub Pull Request Url.')
+    test(`returns an empty string for ${prUrl}`, () => {
+      expect(githubPR.validatePRUrl(prUrl)).toBe('')
     })
   })
 })
 
 describe('gets the API endpoint for a prUrl', () => {
   validPrUrls.forEach(({ prUrl, apiEndpoint }) => {
-    test(`throws for ${prUrl}`, () => {
+    test(`returns the PR API endpoint for ${prUrl}`, () => {
       expect(githubPR.prUrlToApiEndpoint(prUrl).href).toBe(apiEndpoint)
     })
   })
